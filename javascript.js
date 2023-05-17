e3e882ab9e67bef4272c14ed3b420e320b24c3e0
jenny-acero <jenny_pao2@hotmail.com>
2023-05-17 17:06:15 -0500

    se adiciona la descripcion de cada raza

--- javascript.js
+++ javascript.js
@@ -3,20 +3,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
 const razasDePerros = [
     {
-      nombre: "CHIHUAHUA",
-      imagenURL: "imagenes/descarga.jpg"
+        nombre: "CHIHUAHUA",
+        imagenURL: "imagenes/descarga.jpg",
+        descripcion:"Su origen es de mexico del estado de Chihuahua, un adulto suele medir entre 15 y 23 cm y pesar de 1,8 a 2,7 kg. Si no recibe una eduacación apropiada puede llegar a ser nervioso, ladrador y ruidoso." 
     },
     {
         nombre: "SHIH TZU",
-        imagenURL: "imagenes/shih_tzu.jpg" 
+        imagenURL: "imagenes/shih_tzu.jpg",
+        descripcion:"Originario del Tíbet.Es un perro inteligente y muy cariñoso pero de caracter testarudo por esta razón debe tener pautas de obedencia temprana."
     },
     {
         nombre: "BEAGLE",
-        imagenURL: "imagenes/beagle.jpg"  
+        imagenURL: "imagenes/beagle.jpg",
+        descripcion:"Miden de 33 a 40 cm y pesan de 10 o 11 kg.Les encanta convivir con niños lo contrario que con los gatos.No es un perro guardian."
     },
     {
         nombre: "BORDER COLLIE",
-        imagenURL: "imagenes/border_collie.jpg"  
+        imagenURL: "imagenes/border_collie.jpg",
+        descripcion:""
     },
     {
         nombre: "PINSCHER MINIATURA",
@@ -68,13 +72,24 @@ const razasDePerros = [
       description.textContent = raza.descripcion;
       
       cardBody.appendChild(title);
+      cardBody.appendChild(description);
+
       
       card.appendChild(image);
       card.appendChild(cardBody);
       container.appendChild(card); 
+
+    card.addEventListener('mouseenter', () => {
+        description.style.display = 'block';
       
     });
-  }
-  generarTarjetas();
 
+    card.addEventListener('mouseleave', () => {
+        description.style.display = 'none';
+
+  });
+
+});
+}
+generarTarjetas();
 });
