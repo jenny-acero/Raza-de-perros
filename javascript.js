29db85bcfa6e1512feeb43a8f335b0fe70d76645
jenny-acero <jenny_pao2@hotmail.com>
2023-05-19 11:36:42 -0500

    agregar url de nueva pagina

--- javascript.js
+++ javascript.js
@@ -5,46 +5,62 @@ const razasDePerros = [
     {
         nombre: "CHIHUAHUA",
         imagenURL: "imagenes/descarga.jpg",
-        descripcion:"Su origen es de mexico del estado de Chihuahua, un adulto suele medir entre 15 y 23 cm y pesar de 1,8 a 2,7 kg. Si no recibe una eduacación apropiada puede llegar a ser nervioso, ladrador y ruidoso." 
+        descripcion:"Su origen es de    mexico del estado de Chihuahua, un adulto suele medir entre 15 y 23 cm y pesar de 1,8 a 2,7 kg. Si no recibe una eduacación apropiada puede llegar a ser nervioso, ladrador y ruidoso.",
+        url: "/chihuahua.html"
     },
     {
         nombre: "SHIH TZU",
         imagenURL: "imagenes/shih_tzu.jpg",
-        descripcion:"Originario del Tíbet.Es un perro inteligente y muy cariñoso pero de caracter testarudo por esta razón debe tener pautas de obedencia temprana."
+        descripcion:"Originario del Tíbet. Es un perro inteligente y muy cariñoso pero de caracter testarudo por esta razón debe tener pautas de obedencia temprana.",
+        url: "/shih-tzu.html"
     },
     {
         nombre: "BEAGLE",
         imagenURL: "imagenes/beagle.jpg",
-        descripcion:"Miden de 33 a 40 cm y pesan de 10 o 11 kg.Les encanta convivir con niños lo contrario que con los gatos.No es un perro guardian."
+        descripcion:"Miden de 33 a 40 cm y pesan de 10 o 11 kg. Les encanta convivir con niños lo contrario que con los gatos.No es un perro guardian.",
+        url: "/beagle.html"
     },
     {
         nombre: "BORDER COLLIE",
         imagenURL: "imagenes/border_collie.jpg",
-        descripcion:""
+        descripcion:"Los border collie siempre están listos para trabajar y se muestran atentos, activos y vigilantes.Los machos adultos miden unos 53 cm y la altura de las hembras es algo inferior. Suelen pesar entre 14 y 20 kg.",
+        url: "/border-collie.html"
     },
     {
         nombre: "PINSCHER MINIATURA",
-        imagenURL: "imagenes/pinscher_miniatura.jpg"  
+        imagenURL: "imagenes/pinscher_miniatura.jpg",
+        descripcion:"El Pinscher miniatura es un perro elegante. Los adultos miden entre 25 y 30 cm y pesan de 4 a 6 kilos.Suele ser un buen perro guardián para la casa.", 
+        url: "/pinscher-miniatura.html"
     },
     {
         nombre: "BICHON MALTES",
-        imagenURL: "imagenes/bichon_maltes.jpg"  
+        imagenURL: "imagenes/bichon_maltes.jpg",
+        descripcion:"Los machos y las hembras adultos miden entre 23 y 28 cm y pesan de 3 a 6 kg. les encanta la vida familiar.", 
+        url: "/bichon-maltes.html"
     },
     {
         nombre: "COCKER SPANIEL INGLES",
-        imagenURL: "imagenes/cocker_spaniel_ingles.jpg"  
+        imagenURL: "imagenes/cocker_spaniel_ingles.jpg",
+        descripcion:"Su origen es Español. Los machos adultos miden entre 39 y 41 cm y las hembras, entre 38 y 39 cm. Pesan entre 13 y 14,5 kg.",  
+        url: "/cocker-spaniel.html"
     },
     {
         nombre: "PERRO PERUANO",
-        imagenURL: "imagenes/perro_peruano.jpg"  
+        imagenURL: "imagenes/perro_peruano.jpg",
+        descripcion:"Es una raza de perro sin pelo originaria del Perú. Genéticamente tiene un síndrome de hipoplasia ectodérmica, lo cual significa que posee piel cálida que al entrar en contacto con la piel humana la puede calentar",  
+        url: "/perro-peruano.html"
     },
     {
         nombre: "FRENCH PODDLE",
-        imagenURL: "imagenes/french_poddle.jpg"  
+        imagenURL: "imagenes/french_poddle.jpg",
+        descripcion:"Los ejemplares grandes adultos miden un mínimo de 38 cm y pesan entre 20 y 32 kilos. Es un perro jovial y cariñoso que será un compañero muy gratificante. ", 
+        url: "/french-poddle.html"
     },
     {
         nombre: "LABRADOR",
-        imagenURL: "imagenes/labrador.jpg"  
+        imagenURL: "imagenes/labrador.jpg",
+        descripcion:"Se origino en las costas de Terranova en el siglo XVII. Los machos adultos miden entre 56 y 57 cm y pesan unos 30 kg. Las hembras miden entre 55 y 56 cm y pesan 28 kg.",
+        url: "/labrador.html"
     },
   ];
 
@@ -79,15 +95,10 @@ const razasDePerros = [
       card.appendChild(cardBody);
       container.appendChild(card); 
 
-    card.addEventListener('mouseenter', () => {
-        description.style.display = 'block';
-      
-    });
-
-    card.addEventListener('mouseleave', () => {
-        description.style.display = 'none';
-
-  });
+        const url = document.createElement("a");
+        url.href = raza.url;
+        url.textContent ="Ver más...";
+        cardBody.appendChild(url);
 
 });
 }
